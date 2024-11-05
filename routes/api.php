9c7daaafb7a111b20bcfb6826d288d5133583a5f
nql-afesh <monaalii35094@gmail.com>
2024-11-05 11:42:43 +0200

    create post and comment and likes table

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
